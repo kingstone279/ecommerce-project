a53423468a1e7bb2a2a2ca30103ac7d7896232f8
kingstone279 <joshuamaumbe@gmail.com>
2026-04-13 05:18:19 +0200

    created multiple pages using react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-
+import { Routes, Route } from 'react-router'
 import './App.css'
 import { HomePage } from './pages/HomePages'
 
 function App() {
 
   return (
-    <HomePage />
+    <Routes>
+       <Route path='/' element={<HomePage/>} />
+       <Route path='checkout' />
+    </Routes>
   )
 }
 
