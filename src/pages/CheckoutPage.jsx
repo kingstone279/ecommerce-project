ca3936faddaf0766f2ac85a2584d5f6a67a1c1aa
kingstone279 <joshuamaumbe@gmail.com>
2026-04-15 05:46:49 +0200

    Generate the html for the home page

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,6 +1,8 @@
 import './checkout-header.css'
 import { Link } from 'react-router'
 import './CheckoutPage.css'
+import Logo from '../assets/images/logo.png';
+import MobileLogo from '../assets/images/mobile-logo.png';
 export function CheckoutPage() {
   return (
     <>
@@ -8,17 +10,17 @@ export function CheckoutPage() {
       <div className="checkout-header">
         <div className="header-content">
           <div className="checkout-header-left-section">
-            <a href="/home">
-              <img className="logo" src="images/logo.png" />
-              <img className="mobile-logo" src="images/mobile-logo.png" />
-            </a>
+            <Link to="/">
+              <img className="logo" src={Logo} />
+              <img className="mobile-logo" src={MobileLogo} />
+            </Link>
           </div>
 
           <div className="checkout-header-middle-section">
             Checkout (
-            <a className="return-to-home-link" href="/checkout">
+            <Link className="return-to-home-link" to="/checkout">
               3 items
-            </a>
+            </Link>
             )
           </div>
 
