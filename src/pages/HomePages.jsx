004a59cb5cda445e40a3ee30e211470b48c2053c
kingstone279 <joshuamaumbe@gmail.com>
2026-04-17 04:06:16 +0200

    Get quantity from backend and display

--- src/pages/HomePages.jsx
+++ src/pages/HomePages.jsx
@@ -5,18 +5,23 @@ import "./HomePage.css";
 import { Link } from "react-router";
 export function HomePage() {
   const [products, setProducts] = useState([])
+  const [cart, setCart] = useState([])
   useEffect(() =>{
      axios.get('http://localhost:3000/api/products')
       .then((response) => {
           setProducts(response.data); 
    })
+   axios.get('http://localhost:3000/api/cart-items')
+    .then((response) => {
+       setCart(response.data)
+    })
   }, [])
   
   return (
     <>
       <title>Ecommerce Project</title>
 
-      <Header />
+      <Header cart={cart} />
 
       <div className="home-page">
         <div className="products-grid">
